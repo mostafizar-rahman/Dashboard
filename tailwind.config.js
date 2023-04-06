d4c758e35395d5d42d0537b24f723a9be70123dc
Mostafizur <mostafizurdh2000@gmail.com>
2023-04-06 15:24:51 +0600

    edit config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,12 @@ module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./Components/**/*.{js,ts,jsx,tsx}",
 
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  purge: ['./pages/**/*.{js,jsx}', './components/**/*.{js,jsx}'],
+  purge: ['./pages/**/*.{js,jsx}', './Components/**/*.{js,jsx}'],
   theme: {
     extend: {},
   },
